114c8784ff283bf864e8b5f3a3b8afe1cbabf990
Delstun McCray <87548497+Delstroo@users.noreply.github.com>
2024-01-19 23:03:43 -0800

    adding city search funtions

---
+++ script.js
@@ -0,0 +1,28 @@
+let weather = {
+    "apiKey": "78fbb3341212f3963d9fd4825e25c60a",
+    fetchWeather: function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + this.apiKey)
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed)
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".temp").innerText = temp + "°"; 
+        document.querySelector(".humidity").innerText = "Humidity " + humidity + "%"
+        document.querySelector(".wind").innerText = "Wind Speed: " + speed + "mph"
+    },
+
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+}
+
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+})
